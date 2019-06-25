c5f030f3a3b9339c26b8a39e2dbda8e5fb0cc468
Christopher Greaves <28542943+Chris-Greaves@users.noreply.github.com>
2019-06-25 19:25:18 +0100

    Add test for Zipping a folder

--- ezgz_test.go
+++ ezgz_test.go
@@ -28,6 +28,32 @@ func TestFileSuccessfullyZipped(t *testing.T) {
 	}
 }
 
+func TestFolderSuccessfullyZipped(t *testing.T) {
+	folderToZip := getFolderToZip(t)
+	defer os.RemoveAll(folderToZip)
+
+	outputFile := getOutputFilePath(t)
+	defer os.RemoveAll(outputFile)
+
+	err := ZipToFile(folderToZip, outputFile)
+	if assert.NoError(t, err, "unexpected error occurred when converting folder to zip") {
+		_, err := os.Stat(outputFile)
+		assert.NoError(t, err, "error getting stats of output file")
+	}
+}
+
+func getFolderToZip(t *testing.T) string {
+	testDir, err := ioutil.TempDir("", "ezgz-test-output-dir-")
+	require.NoError(t, err, "unable to create temp file for test")
+
+	file, err := ioutil.TempFile(testDir, "ezgz-test-file-*.txt")
+	require.NoError(t, err, "unable to create temp file for test")
+	defer file.Close()
+	_, err = file.WriteString(fileContents)
+	require.NoError(t, err, "unable to add content to temp file")
+	return testDir
+}
+
 func getFileToZip(t *testing.T) string {
 	file, err := ioutil.TempFile("", "ezgz-test-file-*.txt")
 	require.NoError(t, err, "unable to create temp file for test")
